277679361132949d37e67d42b1ac05bfbbd219a2
ricochet <plushambush@gmail.com>
2013-04-30 10:34:33 +0400

    Track configuration moved out to module

--- rman.py
+++ rman.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 # 50.56.75.58:50006 - arena
+# 50.56.75.58:50002 - forza
 
 
 from circuits.io import File
@@ -19,27 +21,7 @@ from raceman.lib.rmagimanager import RMAGIManager,RMAGIHandler
 from raceman.lib.rmagi import AGI
 from signal import SIGHUP
 from exceptions import AttributeError
-
-config={'arena': 
-        { 'streamip':'50.56.75.58',
-          'streamport':50006,
-           'park':
-                {'1':
-                    {'01':'1','02':'2','03':'3','04':'4','05':'5','06':'6',
-                     '07':'7','08':'8','09':'9','10':'10','11':'11','12':'12',
-                     '13':'13','14':'14','15':'15','16':'16','17':'17','18':'18',
-                     '19':'19','20':'20','21':'21','22':'22','23':'23','24':'24'
-                    },
-                 '2':
-                    {'01':'Sodi 1','02':'Sodi 2','03':'Sodi 3','04':'Sodi4','05':'Sodi 5',
-                     '06':'Sodi6','07':'Sodi7','08':'Sodi8','09':'Sodi9','10':'Sodi 10',
-                     '11':'Sodi 11','12':'Sodi 12'
-                    }
-                }
-        }
-       }
-
-# Where is Sodi10, Sodi13,SOdi14,Sodi15
+from raceman.lib.config import config
 
 class Manager(Component):
     """MAIN manager"""
@@ -66,7 +48,7 @@ class Manager(Component):
 
 (Manager()+
 Debugger(file="/var/log/asterisk/demo.log")+
-TCPClient(channel='rminput',bufsize=8192)+
+TCPClient(channel='rminput')+
 #File(filename='stream0.txt',channel='rminput')+
 LP(channel='rminput')+
 RMStream(channel='rminput')+

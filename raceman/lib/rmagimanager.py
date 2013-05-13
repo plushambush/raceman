7a3dbb45ed1b4d96db5693a104023cc5b4c17052
ricochet <plushambush@gmail.com>
2013-05-13 23:00:13 +0400

    Further code cleaning (replaced spaces with tabs)

--- raceman/lib/rmagimanager.py
+++ raceman/lib/rmagimanager.py
@@ -8,25 +8,33 @@ class RMAGICommand(Event):
 	name='rmagicommand'
 
 class RMAGIManager(EventQueue):
-    @handler("rmtellsaymessage")
-    def _rmtellsaymessage(self,phrase,rmprio):
-        self.fireEvent(EQEnqueueEvent(AGICommand(u"EXEC FESTIVAL \"%s\"" % phrase),rmprio))
+	@handler("rmtellsaymessage")
+	def _rmtellsaymessage(self,phrase,rmprio):
+		self.fireEvent(EQEnqueueEvent(AGICommand(u"EXEC FESTIVAL \"%s\"" % phrase),rmprio))
 
-    @handler("rmtellplayfile")
-    def _rmtellplayfile(self,_file,rmprio):
-        self.fireEvent(EQEnqueueEvent(AGICommand(u"STREAM FILE custom/%s any" % _file),rmprio))
+	@handler("rmtellplayfile")
+	def _rmtellplayfile(self,_file,rmprio):
+		self.fireEvent(EQEnqueueEvent(AGICommand(u"STREAM FILE custom/%s any" % _file),rmprio))
 
+#	@handler("rmtellplaymusic")
+#	def _rmtellplaymusic(self,music,rmprio):
+#		self.fireEvent(EQEnqueueEvent(AGICommand(u"SET MUSIC on raceman" ),rmprio))
+
+#	@handler("rmtellstopmusic")
+#	def _rmtellstopmusic(self,rmprio):
+#		self.fireEvent(EQEnqueueEvent(AGICommand(u"SET MUSIC off"),rmprio))
 
 class RMAGIHandler(EQHandler):
-    @handler("agistartupcomplete")
-    def _onrmagistartupcomplete(self,*args,**kwargs):
-        self.fireEvent(EQHandlerAvailable(self))
+	@handler("agistartupcomplete")
+	def _onrmagistartupcomplete(self,*args,**kwargs):
+		self.fireEvent(EQHandlerAvailable(self))
 
-    @handler("rmagicommand")
-    def _onrmagicommand(self,*args,**kwargs):
-        self.fireEvent(AGICommand(*args,**kwargs))
+	@handler("rmagicommand")
+	def _onrmagicommand(self,*args,**kwargs):
+		self.fireEvent(AGICommand(*args,**kwargs))
 
 
-    @handler("agiready")
-    def _onagiready(self,*args,**kwargs):
-        self.fireEvent(EQHandlerAvailable(self))
+	@handler("agiready")
+	def _onagiready(self,*args,**kwargs):
+		self.fireEvent(EQHandlerAvailable(self))
+        

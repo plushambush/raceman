73cee0627064f2763fb131d19e7a8b53202bbabe
ricochet <plushambush@gmail.com>
2013-10-22 23:38:46 +0400

    Cleaned code - rplaced spaces with tabs

--- raceman/lib/rmagi.py
+++ raceman/lib/rmagi.py
@@ -44,71 +44,76 @@ class AGIHangup(Event):
 	name='agihangup'
 
 class AGI(Component):
-    def __init__(self,*args,**kwargs):
-        super(AGI,self).__init__(*args,**kwargs)
-        self._state=ST_STARTUP
-        self.Input=File(sys.__stdin__,mode="r", channel="stdin").register(self)
-        self.Output=File(sys.__stdout__,mode="w", channel="stdout", encoding='utf-8').register(self)
-        self.LP=LP(channel='stdin').register(self)
-        self._agi_args={}
-        self._response=[]
-        self._answer=None
+	def __init__(self,*args,**kwargs):
+		super(AGI,self).__init__(*args,**kwargs)
+		self._state=ST_STARTUP
+		self.Input=File(fd=sys.__stdin__,mode="r", channel="stdin", encoding='utf-8').register(self)
+		self.Output=File(fd=sys.__stdout__,mode="w", channel="stdout", encoding='utf-8').register(self)
+		self.LP=LP(channel='stdin').register(self)
+		self._agi_args={}
+		self._response=[]
+		self._answer=None
 
-    @handler('line',channel='stdin')
-    def line(self,line):
-        if self._state==ST_STARTUP:
-            """Get args and vars from asterisk at startup"""
-            match=AGIARG.match(line)
-            if match:
-                (name,val)=match.groups()
-                self._agi_args[name]=val
-            elif line=="":
-                self._state=ST_READY
-                self.fireEvent(AGIStartupComplete(self._agi_args))
-            else:
-                raise Exception("AGI: Bad line at startup received:%s" % line)
-        elif self._state==ST_COLLECT:
-            """This is a command result"""
-            self._response.append(line)
-            if line[:3]==self._answer:
-                self.fireEvent(AGIResult(self._answer,None,"\n".join(self._response)))
-                self._response=[]
-                self._answer=None
-                self._state=ST_READY
-                self.fireEvent(AGIReady())
-                sys.stderr.write("Debug: COLLECT mode end\n")
-        elif self._state==ST_BUSY:
-            match=AGIRESPONSE.match(line)
-            if match:
-                (answer,delim,data)=match.groups()
-                self._answer=answer
-                if delim=='-':
-                    self._response=[line]
-                    self._answer=answer
-                    self._state=ST_COLLECT
-                else:
-                    match=AGIKV.match(data)
-                    if match:
-                        self.fireEvent(AGIResult(self._answer,match.group('result'),match.group('extra')))
-                    self._response=[]
-                    self._answer=None
-                    self._state=ST_READY
-                    self.fireEvent(AGIReady())
-            else:
-                pdb.set_trace()
-                raise Exception("AGI: Bad response received:" % line)
+	@handler('line',channel='stdin')
+	def line(self,line):
+		if self._state==ST_STARTUP:
+			"""Get args and vars from asterisk at startup"""
+			match=AGIARG.match(line)
+			if match:
+				(name,val)=match.groups()
+				self._agi_args[name]=val
+			elif line=="":
+				self._state=ST_READY
+				self.fireEvent(AGIStartupComplete(self._agi_args))
+			else:
+				raise Exception("AGI: Bad line at startup received:%s" % line)
+		elif self._state==ST_COLLECT:
+			"""This is a command result"""
+			self._response.append(line)
+			if line[:3]==self._answer:
+				self.fireEvent(AGIResult(self._answer,None,"\n".join(self._response)))
+				self._response=[]
+				self._answer=None
+				self._state=ST_READY
+				self.fireEvent(AGIReady())
+				sys.stderr.write("Debug: COLLECT mode end\n")
+		elif self._state==ST_BUSY:
+			match=AGIRESPONSE.match(line)
+			if match:
+				(answer,delim,data)=match.groups()
+				self._answer=answer
+				if delim=='-':
+					self._response=[line]
+					self._answer=answer
+					self._state=ST_COLLECT
+				else:
+					match=AGIKV.match(data)
+					if match:
+						self.fireEvent(AGIResult(self._answer,match.group('result'),match.group('extra')))
+					self._response=[]
+					self._answer=None
+					self._state=ST_READY
+					self.fireEvent(AGIReady())
+			else:
+				pdb.set_trace()
+				raise Exception("AGI: Bad response received:" % line)
 
-        else:
-            raise Exception("AGI: Got response when not in BUSY|COLLECT state (line=%s) (state=%d)\n" % (line, self._state))
-
-
-    @handler("agiresult")
-    def _agiresult(self,answer,result,extra):
-        if int(answer)==200 and int(result)==-1:
-            self.fireEvent(AGIHangup())
+		else:
+			raise Exception("AGI: Got response when not in BUSY|COLLECT state (line=%s) (state=%d)\n" % (line, self._state))
 
 
-    @handler("agicommand")
-    def _agicommand(self,data):
-        self._state=ST_BUSY
-        self.Output.write(str(bytearray(data+ "\n",'utf-8')))
+	@handler("agiresult")
+	def _agiresult(self,answer,result,extra):
+		if int(answer)==200 and int(result)==-1:
+			self.fireEvent(AGIHangup())
+
+	@handler('agicommand',channel='agi')
+	def _agicommand(self,data):
+		self._state=ST_BUSY
+		data1=bytearray(data+u"\n",'utf-8')
+		#pdb.set_trace()
+		self.Output.write(data1)
+		#pdb.set_trace()
+		#self.Output.write((data+"\n").encode('utf-8'))
+		#pdb.trace()
+		

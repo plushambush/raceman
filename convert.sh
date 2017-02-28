752d3c83b7a6c362af246036023fe4c16ae9d43a
Ricochet <plushambush@gmail.com>
2017-02-28 08:35:40 +0300

    Converted all sounds to OGG 8kHz mono

--- convert.sh
+++ convert.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sox  -r 22k -t raw -e signed-integer -b 16 -c 2 $1 -t wav -r 22k -b 16 -c 2 -e signed-integer $2
+sox  -r 22k -t raw -e signed-integer -b 16 -c 2 $1 -t ogg -r 8k -c 1  $2

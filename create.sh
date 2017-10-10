6df606bfdf302846e857c4c98741c7d5739e7968
ricochet <plushambush@gmail.com>
2017-10-10 19:57:27 +0300

    create.sh now accepts external IP address as parameter

--- create.sh
+++ create.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 GIT_COMMIT=$(git rev-parse --short HEAD)
-docker container create  \
-	--publish=10.0.2.5:5060:5060/udp \
-	--publish 10.0.2.5:10000-10040:10000-10040/udp \
-	--add-host=myextip:$(dig +short myip) \
+MYEXTIP=$1
+docker create  \
+	--publish=5060:5060/udp \
+	--publish 10000-10040:10000-10040/udp \
+	--add-host=myextip:$MYEXTIP \
 	--volume /var/log/raceman.log:/var/log/raceman.log \
 	--name raceman \
 	raceman:$GIT_COMMIT
